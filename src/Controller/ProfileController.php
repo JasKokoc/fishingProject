16c2b5f6226722aacf61a6e1182527a4dca266a9
I.Sereda <jaskokoc@yandex.ru>
2024-04-17 17:55:25 +0300

    Авторизация + регистрация + страница Profile

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function index(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $user = $this->getUser();
+
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,23 +3,61 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Faker\Provider\PhoneNumber;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class,['label' => 'John'])
-            ->add('second_name', TextType::class,['label' => 'Smith'])
-            ->add('email', EmailType::class,['label' => 'johnsmith@example.com'])
-            ->add('password', PasswordType::class,['label' => 'qwerty123'])
-        ;
+            ->add('name', TextType::class, [
+                'constraints' =>
+                    [
+                        new NotBlank([
+                            'message' => 'Please enter your name.',
+                        ]),
+                    ],
+            ])
+            ->add('lastName', TextType::class)
+            ->add('email', EmailType::class, [
+                'constraints' =>
+                    [
+                        new Email([
+                        'message' => 'Please enter a valid email address.',
+                        ]),
+                    ],
+            ])
+            ->add('password', PasswordType::class, [
+                'constraints' =>
+                    [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least 6 characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+            ])
+            ->add('phone', TextType::class, [
+                'constraints' =>
+                [
+                    new NotBlank([
+                        'message' => 'Please enter your phone number.',
+                    ])
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
